6af83a86f09d7eaa02bba8cac66790d91ac9d9af
Ivan Drizhiruk <ivan.drizhiruk@gmail.com>
2013-06-16 14:51:23 +0300

    Update

--- src/StopWatchDemo.java
+++ src/StopWatchDemo.java
@@ -7,7 +7,8 @@ import java.util.logging.Logger;
 import com.google.common.base.Stopwatch;
 
 public class StopWatchDemo {
-	private final static Logger LOGGER = Logger.getLogger(StopWatchDemo.class.getCanonicalName());
+	private final static Logger LOGGER = Logger.getLogger(StopWatchDemo.class
+			.getCanonicalName());
 
 	public static void doSomethingJustToBeDoingIt(
 			final int numberOfTimesToDoNothing) {
@@ -26,21 +27,14 @@ public class StopWatchDemo {
 			final Stopwatch stopwatch) {
 		if (stopwatch.isRunning()) {
 			out.println("WARNING! Your stopwatch is still running!");
-		} else
-		{
+		} else {
 			out.println(numberLoops + " loops required: ");
-			
 			out.println("\t" + stopwatch.toString(6));
-			out.println("\t" + stopwatch.elapsedMillis()
-					+ " elapsed milliseconds.");
-			out.println("\t" + stopwatch.elapsedTime(TimeUnit.MINUTES)
-					+ " minutes");
-			out.println("\t" + stopwatch.elapsedTime(TimeUnit.SECONDS)
-					+ " seconds");
-			out.println("\t" + stopwatch.elapsedTime(TimeUnit.MILLISECONDS)
-					+ " milliseconds");
-			out.println("\t" + stopwatch.elapsedTime(TimeUnit.NANOSECONDS)
-					+ " nanoseconds");
+			out.println("\t" + stopwatch.elapsedMillis() + " elapsed milliseconds.");
+			out.println("\t" + stopwatch.elapsedTime(TimeUnit.MINUTES) + " minutes");
+			out.println("\t" + stopwatch.elapsedTime(TimeUnit.SECONDS) + " seconds");
+			out.println("\t" + stopwatch.elapsedTime(TimeUnit.MILLISECONDS) + " milliseconds");
+			out.println("\t" + stopwatch.elapsedTime(TimeUnit.NANOSECONDS) + " nanoseconds");
 		}
 	}
 
