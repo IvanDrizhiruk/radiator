fd9a9ced3046bafcf803ef3085d6341c83fcd8d8
Ivan Drizhiruk <ivan.drizhiruk@gmail.com>
2013-07-03 08:05:53 +0300

    Add dependency to SpiraTestSync.

---
+++ lib/mvn_install.bat
@@ -0,0 +1,1 @@
+call mvn install:install-file -Dfile=xp-st-sync-util-1.0.5.7-SNAPSHOT.jar -DgroupId=ua.dp.ardas.tools.sync -DartifactId=xp_st_sync -Dversion=1.0.5.7-SNAPSHOT -Dpackaging=jar -DgeneratePom=true
---
+++ lib/tmp/mvn_install.bat
@@ -0,0 +1,3 @@
+call mvn install:install-file -Dfile=jakarta-oro__V2.0.6.jar -DgroupId=apache-oro -DartifactId=jakarta-oro -Dversion=2.0.6 -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -Dfile=getopt-1.0.13.jar -DgroupId=gnu-getopt -DartifactId=getopt -Dversion=1.0.13 -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -Dfile=java-getopt-1.0.13.jar -DgroupId=gnu -DartifactId=getopt -Dversion=1.0.13 -Dpackaging=jar -DgeneratePom=true
