9e74af07c6b8d5e9bdbbab5fe914fb389e1f6e90
ivan.drizhiruk <ivan.drizhiruk@gmail.com>
2013-07-14 18:23:31 +0300

    Move shapshot from external libreris.

--- lib/mvn_install.sh
+++ lib/mvn_install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-mvn install:install-file -Dfile=xp-st-sync-util-1.0.5.7-SNAPSHOT.jar -DgroupId=ua.dp.ardas.tools.sync -DartifactId=xp_st_sync -Dversion=1.0.5.7-SNAPSHOT -Dpackaging=jar -DgeneratePom=true
-mvn install:install-file -Dfile=ws-st-api-1.0.5.7-SNAPSHOT.jar -DgroupId=ua.dp.ardas.tools.sync -DartifactId=ws-st-api -Dversion=1.0.5.7-SNAPSHOT -Dpackaging=jar -DgeneratePom=true
-mvn install:install-file -Dfile=common-sync-util-1.0.5.7-SNAPSHOT.jar -DgroupId=ua.dp.ardas.tools.sync -DartifactId=common-sync-util -Dversion=1.0.5.7-SNAPSHOT -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=xp-st-sync-util-1.0.5.7-SNAPSHOT.jar -DgroupId=ua.dp.ardas.tools.sync -DartifactId=xp_st_sync -Dversion=1.0.5.7.1 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=ws-st-api-1.0.5.7-SNAPSHOT.jar -DgroupId=ua.dp.ardas.tools.sync -DartifactId=ws-st-api -Dversion=1.0.5.7.1 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=common-sync-util-1.0.5.7-SNAPSHOT.jar -DgroupId=ua.dp.ardas.tools.sync -DartifactId=common-sync-util -Dversion=1.0.5.7.1 -Dpackaging=jar -DgeneratePom=true
